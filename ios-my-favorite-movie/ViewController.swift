358da2116eb49432ba891d1a47be2fef7c07c2ba
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-11-01 19:50:18 +0900

    Initial Commit

---
+++ ios-my-favorite-movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-my-favorite-movie
+//
+//  Created by 황인우 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
