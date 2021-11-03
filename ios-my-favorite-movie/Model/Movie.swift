5f5e5633595dffe21c147b6fa92c97a84fe07c8f
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-11-03 20:04:01 +0900

    feat : Movie 데이터 모델 구현
    네이버 영화 API통해 전달받을 데이터를 출력할 모델 구현

---
+++ ios-my-favorite-movie/Model/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  ios-my-favorite-movie
+//
+//  Created by 황인우 on 2021/11/01.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    let title: String
+    let link: String
+    let image: String
+    let subtitle: String
+    let pubDate: String
+    let director: String
+    let actor: String
+    let userRating: String
+}
