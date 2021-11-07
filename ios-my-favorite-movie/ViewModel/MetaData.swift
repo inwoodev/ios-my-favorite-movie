d2fdd63b66a4d7ccf4525e74a08ebf3fc2519b7b
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-11-07 22:59:18 +0900

    feat: MetaData 생성
    테이블 뷰와 셀에 표시될 데이터 타입 구현

---
+++ ios-my-favorite-movie/ViewModel/MetaData.swift
@@ -0,0 +1,31 @@
+//
+//  MetaData.swift
+//  ios-my-favorite-movie
+//
+//  Created by 황인우 on 2021/11/05.
+//
+
+import UIKit
+
+struct MetaData {
+    let title: String
+    let link: String
+    let image: String
+    let director: String
+    let actor: String
+    let userRating: String
+    var favoriteStatus: FavoriteStatus
+}
+
+struct ContentMetaData {
+    let title: String
+    var image: UIImage?
+    let director: String
+    let actors: String
+    let userRating: String
+    let link: URL
+}
+
+enum FavoriteStatus {
+    case initial, checked, unchecked
+}
